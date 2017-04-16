3cd44c631ac8d03f0ee2ccb58a695f6025c1479c
Miguel de Moura <me@migueldemoura.com>
2017-04-16 01:44:38 +0100

    Fix cron backups

--- deployment/backup-mysql/main.sh
+++ deployment/backup-mysql/main.sh
@@ -8,8 +8,7 @@
 if [ -n "${BACKUP_ON_INIT}" ]; then
     /backup.sh
 elif [ -n "${RESTORE_LATEST_ON_INIT}" ]; then
-    until nc -z ${MYSQL_HOST} ${MYSQL_PORT}
-    do
+    while ! mysqladmin ping -h"${DB_HOST}" -P"${MYSQL_PORT}" --silent; do
         echo "[Note] Waiting for the mysql container"
         sleep 1
     done
@@ -17,7 +16,10 @@ elif [ -n "${RESTORE_LATEST_ON_INIT}" ]; then
     find /backup/* -maxdepth 1 -type f | xargs -d '\n' ls -t | head -n1 | xargs /restore.sh
 fi
 
-echo "0 0 * * * /backup.sh" > /crontab.conf
-crontab  /crontab.conf
+envsubst '${MYSQL_HOST} ${MYSQL_PORT} ${MYSQL_USER} ${MYSQL_PASSWORD} ${MAX_BACKUPS}' < /backup.sh > /backup.sh.new && mv /backup.sh.new /backup.sh
+envsubst '${MYSQL_HOST} ${MYSQL_PORT} ${MYSQL_USER} ${MYSQL_PASSWORD}' < /restore.sh > /restore.sh.new && mv /restore.sh.new /restore.sh
+
+echo -e "0 0 * * * root bash /backup.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/backup-cron
+touch /var/log/cron.log
 echo "[Note] Running cron job"
 exec cron -f
--- deployment/backup-php/main.sh
+++ deployment/backup-php/main.sh
@@ -7,7 +7,7 @@ elif [ -n "${RESTORE_LATEST_ON_INIT}" ]; then
     find /backup/* -maxdepth 1 -type f | xargs -d '\n' ls -t | head -n1 | xargs /restore.sh
 fi
 
-echo "0 0 * * * /backup.sh" > /crontab.conf
-crontab  /crontab.conf
+echo -e "0 0 * * * root bash /backup.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/backup-cron
+touch /var/log/cron.log
 echo "[Note] Running cron job"
 exec cron -f
