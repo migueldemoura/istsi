5a620edd944a978fe4417312195eefe359ee9cfa
Miguel de Moura <me@migueldemoura.com>
2017-07-30 22:35:47 +0100

    Remove empty global middleware declaration

--- app/public/index.php
+++ app/public/index.php
@@ -7,7 +7,6 @@ $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
 require __DIR__ . '/../src/dependencies.php';
-require __DIR__ . '/../src/middleware.php';
 require __DIR__ . '/../src/routes.php';
 
 $app->run();
